77fe5891e5a5f20f050301ae4ba02a8775153136
Miguel Peralta <eudysperalta723@gmail.com>
2019-09-06 13:56:08 -0400

    added cors whatever

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const ProjectsRouter = require('./projectsRouter')
 const ActionsRouter = require('./actionsRouter')
 
 const server = express()
+
 server.use(express.json())
+server.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+});
 server.use('/api/projects', ProjectsRouter)
 server.use('/api/actions', ActionsRouter)
 
